32111ef1322ff72afd287220b246266951dfd2f9
heshan umayanga <heshanu97@gmail.com>
2022-02-12 01:13:08 +0530

    added api acess key using getphoto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoShowComponent } from './photo-show/photo-show.component';
+//add httpclientmodule
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
@@ -12,7 +13,7 @@ import {HttpClientModule} from '@angular/common/http';
     PhotoShowComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,//
     AppRoutingModule,HttpClientModule
   ],
   providers: [],
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//add hrrp
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+      
+  }
+
+  getPhoto(){
+    return this.http.get('https://api/unsplash.com/photos/random',{
+      headers:{
+        Authenticn:' Client-ID MKBag_smWNjRfuWukYN17wQFQbtMKGfENg5TLgv7jaE'
+      }
+    })
+  }
 }
